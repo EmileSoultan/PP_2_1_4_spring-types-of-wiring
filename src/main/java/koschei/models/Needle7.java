9ff08befe3769da025a54c4c03da19f2eb20fbc8
Emile <emabish42@gmail.com>
2023-04-08 16:19:47 +0200

    Связал бины через конструктор, методы и поля

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,11 @@
 package koschei.models;
 
 public class Needle7 {
+    public Needle7(Death8 death) {
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
