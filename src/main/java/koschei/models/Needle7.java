4b9a84c0feca68f46f81bf8f3a0024a91c68db26
Emile <emabish42@gmail.com>
2023-04-20 11:10:44 +0200

    Исправил ошибку правки

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,11 +1,13 @@
 package koschei.models;
 
 public class Needle7 {
+    private final Death8 death;
     public Needle7(Death8 death) {
+        this.death = death;
     }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( ";
+        return ", смерть Кощея на игле :( " + death.toString();
     }
 }
